f0a22be4eb44cf2336ea7e08c7674737314ca2a3
chiragl <chirag.loliyana@gmail.com>
2020-05-05 17:23:35 +0530

    Added spring cloud

--- spring-boot-microservices/src/main/java/com/springboot/poc/SpringBootMicroservicesApplication.java
+++ spring-boot-microservices/src/main/java/com/springboot/poc/SpringBootMicroservicesApplication.java
@@ -2,8 +2,10 @@ package com.springboot.poc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringBootMicroservicesApplication {
 
 	public static void main(String[] args) {
